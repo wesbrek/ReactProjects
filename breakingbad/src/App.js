fc79248e080a0d036d07f25870ba579b79cee4c7
Sergio Alfaro <sergioalfaro2807@gmail.com>
2020-09-07 01:29:59 -0600

    Cards, search and spinner component

--- breakingbad/src/App.js
+++ breakingbad/src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
-
+import Header from './components/ui/Header'
+import CharacterGrid from './components/characters/CharacterGrid'
+import Search from './components/ui/Search'
 const App = () => {
+
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
+
+      setItems(result.data)
+      setIsLoading(false)
+    }
+
+    fetchItems();
+  }, [query])
+
   return (
     <div className="container">
-      Hello
+      <Header />
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
